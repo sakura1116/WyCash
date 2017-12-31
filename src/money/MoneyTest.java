c73840c2fe4d6c2a4c814b8c0bcf1ceed3a3d514
sakura1116 <daisuke17260@gmail.com>
2017-12-31 22:47:03 +0900

    第11章 不要になったら消す

--- src/money/MoneyTest.java
+++ src/money/MoneyTest.java
@@ -19,8 +19,6 @@ class MoneyTest {
 	public void testEquality() {
 		assertTrue(Money.dollar(5).equals(Money.dollar(5)));
 		assertFalse(Money.dollar(5).equals(Money.dollar(6)));
-		assertTrue(Money.franc(5).equals(Money.franc(5)));
-		assertFalse(Money.franc(5).equals(Money.franc(6)));
 		assertFalse(Money.franc(5).equals(Money.dollar(6)));
 	}
 
@@ -37,8 +35,4 @@ class MoneyTest {
 		assertEquals("CHF", Money.franc(1).currency());
 	}
 
-	@Test
-	public void testDifferentClassEquality() {
-		assertTrue(new Money(10, "CHF").equals(new Franc(10, "CHF")));
-	}
 }
