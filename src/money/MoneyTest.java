54059f0cc0d39b0082ca1cd67b0b1f908d79f357
sakura1116 <daisuke17260@gmail.com>
2017-12-31 16:28:50 +0900

    第3章三角測量

--- src/money/MoneyTest.java
+++ src/money/MoneyTest.java
@@ -1,6 +1,8 @@
 package money;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
@@ -14,5 +16,11 @@ class MoneyTest {
 
 		product = five.times(3);
 		assertEquals(15, product.amount);
+	}
+
+	@Test
+	public void testEquality() {
+		assertTrue(new Dollar(5).equals(new Dollar(5)));
+		assertFalse(new Dollar(5).equals(new Dollar(6)));
 	}
 }
